447ef53961fb6ee98b0c65a298c0a245d464d6a3
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2022-12-13 23:21:08 +0000

    fs: Adiciona os ficheiros fs.c e fs.h
    Adiciona os ficheiros para a implementação da interface com o file system

---
+++ src/sistema/fs.c
@@ -0,0 +1,3 @@
+//
+// Created by João Matos on 13/12/2022.
+//
---
+++ src/sistema/fs.h
@@ -0,0 +1,8 @@
+//
+// Created by João Matos on 13/12/2022.
+//
+
+#ifndef PROJETO_FS_H
+#define PROJETO_FS_H
+
+#endif //PROJETO_FS_H
