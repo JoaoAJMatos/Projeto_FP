7ece3799645b7d93ac622fd3b5463488598a0e82
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2022-12-19 15:36:46 +0000

    app: Modifica o método de armazenamento dos dados
    Remove a utilização de Bencode para a serialização dos dados e passa a guardar os dados dos structs diretamente no ficheiro

--- include/app.h
+++ include/app.h
@@ -94,16 +94,15 @@ typedef struct {
     int* numero_participantes_inseridos;    // Número de participantes inseridos
     int* numero_atividadades_inseridas;     // Número de atividades inseridas
     int* numero_de_inscricoes;              // Número de inscrições inseridas
-    t_caminho* caminho_ficheiro_save;       // Path para o ficheiro onde são guardadas as informações do programa
     int  ultimo_save;                       // Último save efetuado
-    char* cor_texto;                        // Cor do texto
-    char* cor_fundo;                        // Cor do fundo
+    char cor_texto[10];                     // Cor do texto
+    char cor_fundo[10];                     // Cor do fundo
 } t_estado_programa;
 
-t_estado_programa* criar_estado_programa(t_participante**, t_atividade**, t_inscricao**, int*, int*, int*, char*, char*);
+t_estado_programa* criar_estado_programa(t_participante**, t_atividade**, t_inscricao**, int*, int*, int*, const char*, const char*);
 void  libertar_estado_programa(t_estado_programa*);
-int   carregar_estado_programa(t_estado_programa*);
-int   guardar_estado_programa(t_estado_programa*);
+t_estado_programa* carregar_estado_programa(char*);
+int   guardar_estado_programa(char*, t_estado_programa*);
 void  mostrar_estado_programa(t_estado_programa*);
 
 
@@ -115,83 +114,5 @@ int procurar_inscricao_por_id(t_inscricao**, int);
 int procurar_participante_por_nif(t_participante**, int);
 int procurar_participante_por_email(t_participante**, char*);
 int procurar_participante_por_telefone(t_participante**, int);
-
-
-
-
-
-/// SERIALIZAÇÃO ///
-
-/**
- * O método de serialização adotado é inspirado no formato Bencode, utilizado pelo protocolo BitTorrent.
- * Este método de serialização foi escolhido por ser simples de implementar (mais fácil do que construir um parser
- * para JSON ou XML) e por não ser afetado pelos problemas inerentes à serialização de dados binários (como Endianness).
- *
- * Tipos de dados compativeis:
- *  - strings de bytes
- *  - Inteiros
- *  - Vetores
- *  - Dicionários
- *  - Floats
- *
- * Os floats não são compatíveis por default no Bencoding, contudo a nossa implementação permite a serialização de floats.
- *
- * Padrão de serialização:
- *
- * INTEIROS: i<numero inteiro>e ou i-<numero inteiro>e
- * STRINGS: <tamanho da string>:<string>
- * VETORES: l<tamanho>&<elemento 1>&<elemento 2>&...&<elemento n>e - onde o "elemento" é um elemento codificado no formato Bencoding
- * DICIONÁRIOS: d<chave 1><valor 1><chave 2><valor 2>...<chave n><valor n>e - onde a "chave" é uma string codificada no formato Bencoding
- *              e o "valor" é um elemento codificado no formato Bencoding
- * FLOATS: f<numero float>e
- *
- * A proibição da utilização de chamdas recursivas a funções dificultou o processo de serialização, porém procurámos
- * implementar a solução da forma mais elegante possível com as restrições que nos foram dadas.
- */
-
-// Serializar
-
-// Tipos base
-char* serializar_string(char*);
-char* serializar_int(int);
-char* serializar_float(float);
-char* serializar_vetor(char**, int);
-char* serializar_dicionario(char**, char**, int);
-
-// Estruturas
-char* serializar_participante(t_participante*);
-char* serializar_atividade(t_atividade*);
-char* serializar_inscricao(t_inscricao*);
-
-// Vetores de estruturas
-char* serializar_participantes(t_participante**, int);
-char* serializar_atividades(t_atividade**, int);
-char* serializar_inscricoes(t_inscricao**, int);
-
-// Estado do programa
-char* serializar_estado_programa(t_estado_programa*);
-
-
-// Deserializar
-
-// Tipos base
-char*  deserializar_string(char*);
-int    deserializar_int(char*);
-float  deserializar_float(char*);
-char** deserializar_vetor(char*);
-char** deserializar_dicionario(char*);
-
-// Estruturas
-t_participante* deserializar_participante(char*);
-t_atividade* deserializar_atividade(char*);
-t_inscricao* deserializar_inscricao(char*);
-
-// Vetores de estruturas
-t_participante** deserializar_participantes(char*);
-t_atividade** deserializar_atividades(char*);
-t_inscricao** deserializar_inscricoes(char*);
-
-// Estado do programa
-t_estado_programa* deserializar_estado_programa(char*);
 
 #endif //PROJETO1_APP_H
